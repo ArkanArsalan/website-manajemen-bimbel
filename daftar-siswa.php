db6f09f990894c48815d6c4134fbc650328ece86
Arkan Arsalan <arkan.arsalan.amanullah@gmail.com>
2023-12-07 22:58:16 +0700

    Add proses daftar siswa baru

---
+++ daftar-siswa.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulir Pendaftaran Siswa</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <style>
+        <?php include 'styles.css'; ?>
+    </style>
+</head>
+
+<html>
+<body>
+    <div class="form-container" id="daftar">
+        <h1>Daftar</h1>
+        <div class="container-fluid">
+            <form autocomplete="off" action="proses-daftar.php" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label for="nama">Nama</label>
+                    <input type="text" name="nama" placeholder="Nama Lengkap Siswa" class="form-control" minlength="3" maxlength="40" required>
+                </div>
+                <div class="form-group">
+                    <label for="umur">Umur</label>
+                    <input type="number" name="umur" placeholder="Umur" class="form-control" min="1" required>
+                </div>
+                <div class="form-group">
+                    <label for="jenis_kelamin">Jenis Kelamin</label><br>
+                    <input type="radio" name="jenis_kelamin" value="L"> Laki-laki<br>
+                    <input type="radio" name="jenis_kelamin" value="P"> Perempuan<br>
+                </div>
+                <div class="form-group">
+                    <label for="alamat">Alamat</label>
+                    <textarea name="alamat" class="form-control" placeholder="Alamat Lengkap Siswa" required></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="no_telp">Nomor Telepon</label>
+                    <input type="tel" name="no_telp" placeholder="Nomor Telepon" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" placeholder="Email" class="form-control" required>
+                </div>
+                <div class="form-group">
+                    <label for="jenjang_sekolah">Jenjang Sekolah</label>
+                    <input type="text" name="jenjang_sekolah" placeholder="Jenjang Sekolah" class="form-control" minlength="1" required>
+                </div>
+                <div class="form-group">
+                    <label for="cabang_bimbel">Cabang Bimbel</label>
+                    <input type="text" name="cabang_bimbel" placeholder="Cabang Bimbel" class="form-control">
+                </div>
+                <div class="form-group">
+                    <label for="kelas">Kelas</label>
+                    <input type="text" name="kelas" placeholder="Kelas" class="form-control">
+                </div>
+                <button name="daftar" type="submit" class="btn btn-primary">Daftar</button>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
---
+++ list-siswa.php
@@ -0,0 +1,62 @@
+<?php include_once("header.php")?>  
+<?php include("database.php")?>
+<div class="list-siswa-container">
+    <h1>List Daftar Siswa</h1>
+
+    <div class="container" style="margin-top:20px; margin-bottom: 40px; margin-right: 0; margin-left: 0; width: 100%">
+        <div class="col custyle">
+            <table class="table table-striped custab">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nama</th>
+                            <th>Jenis Kelamin</th>
+                            <th>Alamat</th>
+                            <th>No telp</th>
+                            <th>Email</th>
+                            <th>Jenjang Sekolah</th>
+                            <th>Cabang Bimbel</th>
+                            <th>Kelas</th>
+                            <th>Tindakan</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                            $sql = "SELECT * FROM siswa";
+                            $query = mysqli_query($db, $sql);
+
+                            while ($siswa = mysqli_fetch_array($query)) {
+                                echo "<tr>";
+
+                                echo "<td>" . $siswa['id_siswa'] . "</td>";
+                                echo "<td>" . $siswa['nama'] . "</td>";
+                                echo "<td>" . $siswa['jenis_kelamin'] . "</td>";
+                                echo "<td>" . $siswa['alamat'] . "</td>";
+                                echo "<td>" . $siswa['no_telp'] . "</td>";
+                                echo "<td>" . $siswa['email'] . "</td>";
+                                echo "<td>" . $siswa['jenjang_sekolah'] . "</td>";
+                                echo "<td>" . $siswa['cabang_bimbel'] . "</td>";
+                                echo "<td>" . $siswa['kelas'] . "</td>";
+
+                                echo "<td class='text-center'>";
+                                echo "<a class='btn btn-info btn-xs' href='form-edit.php?id=" . $siswa['id_siswa'] . "' ><span class='glyphicon glyphicon-edit'></span>Edit</a> | ";
+                                echo "<a class='btn btn-danger btn-xs' href='hapus.php?id=" . $siswa['id_siswa'] . "'><span class='glyphicon glyphicon-remove'></span>Hapus</a>";
+                                echo "</td>";
+
+                                echo "</tr>";
+                            }
+                        ?>
+                    </tbody>
+                </table>
+                <p style="font-weight : bolder">Total : <?php echo mysqli_num_rows($query) ?></p>
+            </table>
+            <p style="text-align: right; margin:15px">
+                <a href="daftar-siswa.php" class="btn btn-primary btn-xs col-md-3">Tambah Baru</a>
+            </p>
+            <p style="font-weight : bolder"></p>
+        </div>
+
+    </div>
+</div>
+<?php include_once("footer.php")?>  
